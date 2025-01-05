6e36bc4eab304fdcd77476e8707cfced906e971b
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2025-01-06 08:36:55 +0900

    update deployment script to change release directory

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 # configuration
 MOEULE_NAME="rmdsstore"
-RELEASE_DIR=/Users/keiichi/home/bin
+RELEASE_DIR=/usr/local/bin
 
 # deploy
 cp -p ${SCRIPT_DIR}/../target/release/$MOEULE_NAME $RELEASE_DIR
