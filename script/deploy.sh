6b25aa3c92f14cd1b5e4371bc7c57ab5a918d36f
Keiichi Tsuda <keiichi.tsuda@gmail.com>
2025-01-13 10:21:49 +0900

    add build step to deploy script for release version

--- script/deploy.sh
+++ script/deploy.sh
@@ -5,5 +5,8 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 MOEULE_NAME="rmdsstore"
 RELEASE_DIR=/usr/local/bin
 
+# build
+(cd ${SCRIPT_DIR}/.. && cargo build --release)
+
 # deploy
 cp -p ${SCRIPT_DIR}/../target/release/$MOEULE_NAME $RELEASE_DIR
